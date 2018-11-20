171ead07fc4dc0c7993c0e5059d3f4bd3320da4f
Vinar Polina <pelogea2008@gmail.com>
2018-11-20 12:10:15 +0300

    Добавление исключения

--- laba2/laba2/laba2.cpp
+++ laba2/laba2/laba2.cpp
@@ -1,36 +1,24 @@
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include <string> 
+#include <iostream> 
+#include <fstream> 
+#include <sstream> 
 
 using namespace std;
 
-//проверяем, является ли гласной
+//проверяем, является ли гласной 
 bool isAlpfa(char var) {
 	switch (var) {
 	case 'a':
 		return true;
-	case 'A':
-		return true;
 	case 'y':
-		return true;
-	case 'Y':
 		return true;
 	case 'e':
 		return true;
-	case 'E':
-		return true;
 	case 'i':
-		return true;
-	case 'I':
 		return true;
 	case 'o':
 		return true;
-	case 'O':
-		return true;
 	case 'u':
-		return true;
-	case 'U':
 		return true;
 	default:
 		return false;
@@ -39,16 +27,27 @@ bool isAlpfa(char var) {
 
 int main() {
 	setlocale(LC_ALL, "");
-	ifstream in("input.txt");
-	string word;//строка
-	stringstream ss;
-	while (in >> word) {//считываем по предложению
-						//проверяем на гласность
-		if (word.size() > 0 && isAlpfa(word[0])) {
-			ss << word << ' ';//выводим в поток
+	ifstream in;
+	try
+	{
+		in.open("input.txt");
+		if (!in.is_open()) {
+			throw 505;
+		}
+		string word;//строка 
+		stringstream ss;
+		while (in>> word) {//считываем по предложению 
+						   //проверяем на гласность 
+			if (word.size() > 0 && isAlpfa(word[0])) {
+				ss<< word<< ' ';//выводим в поток 
+			}
+			cout<< ss.str();// выводим предложения 
+			in.close();//закрываем файл 
 		}
 	}
-	cout << ss.str();// выводим предложения
-	in.close();//закрываем файл										   
-	system("pause>>void");
+	catch (const int &ex)
+	{
+		cout<< "Ошибка открытия файла!"<< endl<< "Код ошибки: #"<< ex<< endl;
+	}
+	system("pause»void");
 }
